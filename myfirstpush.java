4b4f4ae707cbd3e298667ba73eadaab3e204040c
komurebi <42819974+komurebi@users.noreply.github.com>
2018-09-03 12:12:05 -0400

    Add files via upload

---
+++ myfirstpush.java
@@ -0,0 +1,7 @@
+public class myfirstpush {
+    public static void main (String[] args) {
+        System.out.println("My favorite color is white.");
+        System.out.println("My season is spring.");
+        System.out.println("My time of day is the evening.");
+    }
+}
